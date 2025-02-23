23408681ffd09b0f66fb7052619bd5834686aec5
DomZem <dominik.zemlik.dev@gmail.com>
2025-02-23 21:06:21 +0100

    feat: udpated schema for region model

--- src/common/validations/region/region.ts
+++ src/common/validations/region/region.ts
@@ -2,7 +2,10 @@ import { RegionScalarSchema } from "~/zod-schemas/models";
 
 export const regionSchema = RegionScalarSchema;
 
-export const regionUpdateSchema = regionSchema.omit({});
+export const regionUpdateSchema = regionSchema.omit({
+  created_at: true,
+  updated_at: true,
+});
 
 export const regionCreateSchema = regionUpdateSchema.omit({
   id: true,
