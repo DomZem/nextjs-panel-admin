c93247f6d720ce8c147adbec1bc53fefdc3a3418
DomZem <dominik.zemlik.dev@gmail.com>
2025-02-23 15:42:58 +0100

    fix(test): fixed issue with running playwright tests while running jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,8 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
+
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
