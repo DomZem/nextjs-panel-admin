8553a71aa0f6f5157ccc6b10b27b338f73e934d8
DomZem <dominik.zemlik.dev@gmail.com>
2025-02-26 22:22:40 +0100

    fix: fixed issues with running tests

--- jest.config.ts
+++ jest.config.ts
@@ -8,12 +8,17 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config: Config = {
+  verbose: true,
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
 
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/src/$1",
+  },
+
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
